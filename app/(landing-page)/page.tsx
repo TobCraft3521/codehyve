679d531df9c56afd77536c097374b0938c0b33fc
TobCraft3521 <tobiashackenberg@outlook.de>
2024-05-28 17:57:10 +0200

    initial commit

---
+++ app/(landing-page)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Image src="/codehyve.svg" alt="Vercel Logo" width={72} height={16} />
+    </div>
+  )
+}
